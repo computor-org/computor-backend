bac7b8f57f0b29e326bfd123b35d292891d87ef7
Maximilian Mandlez <maximilian.mandlez@gmx.net>
2025-10-10 01:49:45 +0200

    fix: api docker startup script.

--- docker/api/startup.bash
+++ docker/api/startup.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-
 # Run Alembic migrations
 echo "Applying Alembic migrations..."
 cd computor-backend/src/computor_backend && alembic upgrade head
-#export PYTHONPATH=$PWD:$PYTHONPATH
 
 echo "Initializing system data..."
 python scripts/initialize_system_data.py
 
-cd ..
-# Start the server
-python server.py
+# Go back to home directory before starting server
+cd /home/uvicorn
+
+# Start the server from the correct directory
+python computor-backend/src/server.py
