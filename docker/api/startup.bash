d976f4e2e697261977f97e3a59d1542caf3fe913
Maximilian Mandlez <maximilian.mandlez@gmx.net>
2025-10-10 02:14:19 +0200

    fix: prod api startup.

--- docker/api/startup.bash
+++ docker/api/startup.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+# Start from home directory
+cd /home/uvicorn
+
 # Run Alembic migrations
 echo "Applying Alembic migrations..."
-cd computor-backend/src/computor_backend && alembic upgrade head
+cd computor-backend/src/computor_backend && alembic upgrade head && cd /home/uvicorn
 
 echo "Initializing system data..."
-python scripts/initialize_system_data.py
-
-# Go back to home directory before starting server
-cd /home/uvicorn
+cd computor-backend/src/computor_backend && python scripts/initialize_system_data.py && cd /home/uvicorn
 
 # Start the server from the correct directory
 python computor-backend/src/server.py
